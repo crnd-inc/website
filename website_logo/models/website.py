41e923534dfa953364b107fcb525114eeacd8433
ernesto <ernesto.tejeda87@gmail.com>
2019-05-09 18:12:10 -0400

    [MIG] website_logo: Migration to 12.0

--- website_logo/models/website.py
+++ website_logo/models/website.py
@@ -4,7 +4,4 @@ from odoo import fields, models
 class Website(models.Model):
     _inherit = 'website'
 
-    logo = fields.Binary(
-        string="Website logo",
-        help="This field holds the logo for this website, showed in header. "
-             "Recommended size is 180x50")
+    logo = fields.Binary()
